56d24bd1e353493f307cdbf60a68642cf8db1f85
Samuelguilhermeb <99374269+Samuelguilhermeb@users.noreply.github.com>
2022-03-02 14:58:57 -0300

    primeiro commit

---
+++ src/App.java
@@ -0,0 +1,24 @@
+
+
+import java.util.Scanner;
+import java.util.StringJoiner;
+
+public class App; {
+
+        
+    
+      double base, altura, areaRetangulo;
+
+      base = 34;
+
+      System.out.println(" Informe o valor da altura: ");
+      altura = 57;
+
+      areaRetangulo = base * altura;
+
+      System.out.println(" A area de um retangulo de base =" + base);
+      System.out.println(" e altura" + altura);
+      System.out.println(" é " + areaRetangulo);
+
+    }
+}
